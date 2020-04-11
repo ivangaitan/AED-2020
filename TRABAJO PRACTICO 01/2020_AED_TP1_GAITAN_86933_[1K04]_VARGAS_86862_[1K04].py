d30a25c730b93e0a16cb015793c3d1b72c91230e
Nachovargas93 <30438965+Nachovargas93@users.noreply.github.com>
2020-04-11 20:09:31 -0300

    Replantear_if

--- TRABAJO PRACTICO 01/2020_AED_TP1_GAITAN_86933_[1K04]_VARGAS_86862_[1K04].py
+++ TRABAJO PRACTICO 01/2020_AED_TP1_GAITAN_86933_[1K04]_VARGAS_86862_[1K04].py
@@ -1,24 +1,14 @@
-"""""
-TRABAJO PRACTICO 01 DE AED 2020.
-
+""""" TRABAJO PRACTICO 01 DE AED 2020.
 El programa debe cargar por teclado los datos generales del paciente y los datos que describen los síntomas que presenta ese paciente, y en función de esos datos informar el siguiente cuadro de diagnóstico:
-
 Si es un caso sospechoso.
-
 Si es un caso autóctono:
-
 Presenta al menos 2 de los síntomas respiratorios consultados.
-
 No ha estado en contacto con casos confirmados ni tiene historial de viaje fuera del país, pero estuvo en zonas de transmisión local.
-
 Si es un caso NO sospechoso, pero pertenece al grupo de riesgo porque el paciente es mayor a 60 años.
-
 La carga de datos debe hacerse de forma que se evite el ingreso de datos innecesarios, descartando rápidamente los casos que no sean sospechosos a medida que los datos se van cargando. Para ello, el esquema de carga sugerido puede ser el siguiente:
 
 1.) En primer lugar, cargar la edad,  la temperatura corporal y un valor que indique si el paciente tiene neumonía ya evidenciada. En caso de efectivamente padecer neumonía, el caso es sospechoso sin tener en cuenta las demás variables. Queda a criterio del estudiante la forma en que cargará por teclado el indicador de neumonía.
-
 2.) Si el paciente no tiene neumonía, pero presenta fiebre (temperatura corporal mayor a 37 grados), continuar con la carga de valores que indiquen los síntomas respiratorios (tos, odinofagia o dificultad respiratoria) que se ven en la tabla del Ministerio. Al hacer la carga, tenga en cuenta que un paciente puede tener más de uno de esos síntomas a la vez, y debe cargar entonces tres indicadores: uno para tos, uno para odinofagia, y otro para dificultad respiratoria. Queda a criterio del estudiante la forma en que cargará por teclado los tres indicadores mencionados.
-
 3.) Habiendo cargado los datos del punto 2, si hay fiebre y al menos uno de los síntomas respiratorios, cargar lo siguiente (en todos los casos, se deja para el estudiante determinar la forma en que cargará los indicadores solicitados):
 
 Un valor que indique si el paciente es personal de salud.
@@ -38,9 +28,11 @@ neumonia = int(input('En caso de que el paciente tenga neumonia, marque "1". Si 
 
 fiebre = tempCorp > 37
 
-if neumonia == 1:
-    alerta1 = 'El paciente es un caso SOSPECHOSO de COVID-19'
-elif neumonia == 2:
+alerta1 = str('El paciente es un caso SOSPECHOSO de COVID-19.')
+alerta2 = str('El paciente es un caso Autoctono..')
+alerta3 = str('Caso riesgoso, paciente mayor a 60 años.')
+
+if neumonia == 2:
     if fiebre:
         #carga variables sintomas
         tos = int(input('En caso de que el paciente tenga Tos, marque "1". Si el paciente no tiene Tos marque "2": '))
@@ -51,11 +43,20 @@ elif neumonia == 2:
             contactoCasoConfirmado = int(input('Si el paciente estuvo en contacto con algun caso confirmado de COVID-19, marque "1", si no lo es, marque "2": '))
             viajExt = int(input('Si el paciente tiene registro de viaje al exterior en los ultimos 14 dias, marque "1", si no lo es, marque "2": '))
             transLocal = int(input('Si el paciente estuvo en zonas nacionales con casos de transmision local confirmada, marque "1", si no lo es, marque "2": '))
-                    
+            alerta1
+        elif tos == 1 and odinofagia == 1 or difRespiratoria == 1 and transLocal ==1:
+            alerta2
+    elif edad > 60:
+        alerta3
+elif neumonia == 1:
+    alerta1
+
 else:
     print('Por favor, ingrese una opcion valida.')
 
-
+print(alerta1)
+print(alerta2)
+print(alerta3)
 
 #(tos == 1 or odinofagia == 1 or difRespiratoria == 1)
 
