8e85af5b61ecb108ce8bfd3f654e2eb68eca8613
ivangaitan <ivangaitan.kp@gmail.com>
2020-05-26 15:38:34 -0300

    actualizacion

--- TRABAJO PRACTICO 02/2020_AED_TP2_GAITAN_86933_[1K04]_VARGAS_86862_[1K04].py
+++ TRABAJO PRACTICO 02/2020_AED_TP2_GAITAN_86933_[1K04]_VARGAS_86862_[1K04].py
@@ -1,46 +1,109 @@
 import random
 
-# pide ingresar un usuario
-cuenta_usuario  = str(input("Ingrese su usuario: ")).capitalize()
+cuenta_usuario = str(input("Ingrese su usuario: ")).capitalize()
 arroba = cuenta_usuario.count("@")
 intentos = 0
 
-# verifica que el usuario sea valido y si es invalido da 3 intentos
-while '..' in cuenta_usuario or cuenta_usuario.startswith(".") == True or cuenta_usuario.endswith(".") == True or arroba > 1 or arroba == 0 or cuenta_usuario.startswith("@") == True or cuenta_usuario.endswith("@") == True or not cuenta_usuario:
+# Validar cuenta...
+while '..' in cuenta_usuario or cuenta_usuario[0] == '.' or cuenta_usuario.endswith(".") == True or arroba > 1 or arroba == 0 or cuenta_usuario.startswith("@") == True or cuenta_usuario.endswith("@") == True or not cuenta_usuario:
     intentos += 1
     if intentos == 3:
         print ("Demasiados intentos")
         break;
     cuenta_usuario = str(input("El usuario ingresado es incorrecto, ingrese nuevamente el usuario: "))
     arroba = cuenta_usuario.count("@")
-
-# pide al usuario la cantidad de pacientes a analizar
-cantidad_pacientes = int(input("Ingrese la cantidad de pacientes a procesar: "))
-paciente = [1]
-
-# definimos los datos de cada paciente que necesita el usuario
-resultado_test = ["Test Positivo", "Test Negativo"]
-region = ["Capital", "Gran Cordoba", "Norte", "Sur"]
-contacto_casos_confirmados = ["Tuvo contacto con casos confirmados", "No tuvo contacto con casos confirmados"]
-personal_salud = ["Es personal de salud", "No es personal de salud"]
-viajo_exterior = ["Viajo al exterior", "No viajo al exterior"]
-
-# agrega la cantidad de pacientes a la lista paciente
-for p in paciente:
-    p += 1
-    paciente.extend([p])
-    if p == cantidad_pacientes:
-        break;
-
-# a cada paciente le crea datos aleatorios y los agrega a una lista
-for i in paciente:
-    edad = random.randint(2, 100)
-    i = ["paciente: " + str(paciente.index(i) + 1), str(edad) + " años", random.choice(resultado_test), "Region: " + random.choice(region), random.choice(contacto_casos_confirmados), random.choice(personal_salud), random.choice(viajo_exterior)]
-    print(i)
+# Crear funciones
 
 
+def cantConfirmados(a):
+    if a == 'Positivo':
+        return True
+    return False
 
 
+def porcentaje(b, c):
+    por = (c * 100) / b
+    return por
 
 
+def promedio(a, b):
+    prom = a / b
+    return prom
 
+
+# Solicitar al usuario que ingrese la cantidad de pacientes a procesar...
+n = int(input('Ingrese la cantidad de pacientes que desea procesar: '))
+test = ('Positivo', 'Negativo')
+regiones = ('Capital', 'Gran Córdoba', 'Norte', 'Sur')
+# contactoCaso = ('Si', 'No')
+# perSalud = ('Si', 'No')
+viajExt = perSalud = contactoCaso = ('Si', 'No')
+
+# Definir variables a utilizar..
+confirmados = porcentajeConf = acEdad = contEdad = cantPer = acEdad1 = autoc = 0
+menor = None
+regioncapital = regionGC = regionNorte = regionSur = 0
+
+# Por cada paciente sospechoso, generar de manera aleatoria
+for i in range(n):
+    edad = random.randint(16, 99)
+    resulTest = random.choice(test)
+    regionPac = random.choice(regiones)
+    contactoCasoCon = random.choice(contactoCaso)
+    personalSalud = random.choice(perSalud)
+    ViajeExt = random.choice(viajExt)
+    print(edad, resulTest, regionPac, contactoCasoCon, personalSalud, ViajeExt)
+
+    # Caso autoctono
+    if resulTest == 'Positivo' and contactoCasoCon == 'No' and personalSalud == 'No' and ViajeExt == 'No':
+        autoc += 1
+        # 5. Menor edad entre los casos autóctonos.
+        if i == 0 or edad < menor:
+            menor = edad
+
+    # 1. Cantidad de casos confirmados (test positivo) y porcentaje sobre el total de casos.
+    if cantConfirmados(resulTest):
+        confirmados += 1
+
+    # 2. Edad promedio de los pacientes que pertenecen a grupo de riesgo (para pertenecer al grupo de riesgo el test debe ser negativo y tener más de 60 años).
+    if resulTest == 'Negativo' and edad > 60:
+        contEdad += 1
+        acEdad += edad
+
+    # 3. Cantidad y porcentaje que el personal de salud representa sobre el total de casos.
+    if personalSalud == 'Si':
+        cantPer += 1
+
+    # 4. Edad promedio entre los casos confirmados.
+    acEdad1 += edad
+
+    # 6. Cantidad de casos confirmados por región y porcentaje que representa cada uno sobre el total de casos.
+    if resulTest == 'Positivo' and regionPac == 'Capital':
+        regioncapital += 1
+    elif resulTest == 'Positivo' and regionPac == 'Gran Córdoba':
+        regionGC += 1
+    elif resulTest == 'Positivo' and regionPac == 'Norte':
+        regionNorte += 1
+    elif resulTest == 'Positivo' and regionPac == 'Sur':
+        regionSur += 1
+
+# 1
+porcentajeConf = porcentaje(n, confirmados)
+# 2
+PromEdad = promedio(acEdad, contEdad)
+# 3
+porcentajePer = porcentaje(n, cantPer)
+# 4
+promEdad1 = promedio(acEdad1, n)
+# 6
+porcentajeCapital = porcentaje(n, regioncapital)
+porcentajeGC = porcentaje(n, regionGC)
+porcentajeNorte = porcentaje(n, regionNorte)
+porcentajeSur = porcentaje(n, regionSur)
+
+print('\nCantidad de confirmados: ', confirmados, 'y el porcentaje sobre el total de casos: ', porcentajeConf, '%')
+print('\nEdad promedio de los pacientes que pertenecen a grupo de riesgo: ', PromEdad)
+print('\nLa cantidad de sospechosos que forman parte del personal de salud son', cantPer, 'los cuales representan un ', porcentajePer, '%')
+print('\nEdad promedio entre los casos confirmados: ', promEdad1)
+print('\nMenor edad entre los casos autoctonos: ', menor, 'cantidad de casos: ', autoc)
+print('\nCantidad de casos confirmados por region: \nRegion Capital:', regioncapital, 'los cuales representan un', porcentajeCapital, '%')
--- TRABAJO PRACTICO 02/sucesiones.py
+++
@@ -1,6 +0,0 @@
-
-while True:
-    n = int(input("posicion: "))
-    numero = 2 * n + 1
-    print(numero)
-
